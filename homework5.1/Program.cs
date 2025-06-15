86fb0e1d5f6c9cd3806f66e37eaf12f196f7182c
Oleksandr <sanyalisnyak@gmail.com>
2025-06-15 08:46:48 +0300

    Added homework 5.1 and 5.2

---
+++ homework5.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+long firstNumber = 0; 
+long secondNumber = 0; ;
+
+Console.WriteLine("enter two numbers : ");
+firstNumber = Convert.ToInt64(Console.ReadLine());
+secondNumber = Convert.ToInt64(Console.ReadLine());
+
+try
+{
+    if (secondNumber == 0)
+        throw new DivideByZeroException("Division by zero");
+    else Console.WriteLine (firstNumber / secondNumber);
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Division by zero!");
+}
+catch (Exception)
+{
+    Console.WriteLine("Unknown Exception");
+}
+finally
+{
+    Console.WriteLine("Calculation complete");
+}
---
+++ homework5.2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter numbers:");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine($"You entered a number: {number}");
+}
+else
+{ 
+    Console.WriteLine("Error: The entered value is not a number.");
+}
